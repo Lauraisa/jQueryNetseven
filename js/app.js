f22765c10b9d569d1f180a371e54f1f6ecf8405b
MarinaRH <l_marina1717@hotmail.com>
2017-12-20 15:53:40 -0500

    Modificando cat1

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
     //código a ejecutar cuando el DOM está listo para recibir instrucciones.
     $("#Gato-1").click(function(evento){
-        $('#Gato-1').addClass("medium-kat")
-        $('#Gato-1').parent().removeClass('col-xs-offset-3');
+        $('#Gato-1').toggleClass("medium-kat");
         $('#Gato-1').parent().addClass('margin');
-        
+        $('#Gato-1').parent().toggleClass("nieve");
+
     });
+
+    // $( document.body ).click(function() {
+    //     $( "div:hidden:first" ).fadeIn( "slow" );
+    //   });
     /*trabajando bg y animacion de cat */
     $(".bg-mobil").click(function(evento){
         $(".bg-mobil").toggleClass("back-red");
@@ -13,18 +17,12 @@ $(document).ready(function(){
 
     });
     $( ".box-cat" ).click(function() {
-        $( "#gato-2" ).fadeOut( "slow", function() {
-          // El gatito desaparece
+        $( "#gato-2" ).slideToggle( "slow", function() {
+          
         });
         $( "#gato-2" ).show( "slow", function() {
         });
-        // El gatito aparece
-
-        /* $( "#gatgif" ).show( "slow", function() {
-        });
-        // El gatito aparece
-
-      }); para modificar*/
+        
     
       });
     
